5e419e84fbb3b02d0302bab0e671448f2daab85c
Tina <tinaqt122@gmail.com>
2025-01-21 19:56:36 +0100

    stuff and things

--- dotfiles/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    alias hyprfetch="fastfetch --logo ~/Pictures/hyprchan.png --logo-height 24" 
-    wal -R
-    clear
-    # Commands to run in interactive sessions can go here
-end
